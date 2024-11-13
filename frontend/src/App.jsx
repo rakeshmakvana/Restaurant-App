33e4a9b70438a4474d383967563bec9c94a56dde
dhruvilpatoliya <dhruvilpatoliya3@gmail.com>
2024-11-14 04:08:39 +0530

    feature/adding-dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import React from 'react';
+import Sidebar from './component/sidebar/sidebar';
+import Header from './component/Header/Header';
+import Dashboard from './component/Dashboard/Dashboard';
 
-function App() {
 
+const App = () => {
   return (
     <>
-      
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+      </div>
+      <Header />
+      <Dashboard />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
